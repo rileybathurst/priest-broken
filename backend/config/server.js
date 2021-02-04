0e3db8811c5f7592411651ab5c8878e1c51b860e
Riley <riley@rileybathurst.com>
2021-02-04 13:58:37 -0800

    all the updates

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e1681520c52709ae8e9b0fb726c9b80'),
+    },
+  },
 });
